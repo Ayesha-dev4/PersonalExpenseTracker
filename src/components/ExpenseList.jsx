b1f91e9f8d0a2232ba06078574ff464aab6e162c
Ayesha <237526697+Ayesha-dev4@users.noreply.github.com>
2025-10-16 02:36:15 -0700

    Add modal logic

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,10 +1,13 @@
-
+import { useState, useEffect } from "react";
 import ExpenseCard from "./ExpenseCard"
 import mockExpenseList from "../mockData/mockExpenseList"
 import calculateTotal from "../utils/calculateTotal"
+import { AnimatePresence } from "framer-motion";
+import AddExpense from "./AddExpense";
 
 export default function ExpenseList() {
     const [expenses, setExpenses] = useState(mockExpenseList)
+    const [showModal, setShowModal] = useState(false); // toggle to true to see expense form - false to dismiss
 
     const total = calculateTotal(expenses)
     useEffect(() => {
@@ -21,6 +24,21 @@ export default function ExpenseList() {
 
     return (
           <>
+        <button
+        onClick={() => setShowModal(true)}
+        className="bg-purple-600 text-white px-4 py-2 rounded hover:bg-purple-700 text-sm sm:text-base md:text-lg
+    sm:px-5 sm:py-2.5 md:px-6 md:py-3"
+      >
+        + Add Expense
+      </button>
+
+      {/* AnimatePresence wraps around the conditional render */}
+      <AnimatePresence>
+        {showModal && (
+          <AddExpense onCancel={() => setShowModal(false)} />
+        )}
+      </AnimatePresence>
+
         {expenses.map( (expense, index) => (
             <ExpenseCard key={index} title={expense.title} amount={expense.amount} date={expense.date} category={expense.category} />
         ))}
