35ecf3b50a2a7ee382b53bf99338e5080d59456d
Ayesha <237526697+Ayesha-dev4@users.noreply.github.com>
2025-10-16 06:36:09 -0700

    Add expenseObject

---
+++ src/mockData/expenseObject.js
@@ -0,0 +1,8 @@
+export default function expenseObject(title, date, amount, category) {
+    return {
+        title: title,
+        date: date,
+        amount: amount,
+        category, category
+    }
+}
